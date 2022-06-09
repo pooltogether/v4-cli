74dfc054f887e8ba61e53dd6724bd148bb0f8069
kames <kamescg@gmail.com>
2022-06-09 14:10:15 -0600

    fix(tests): remove

--- src/lib/utils/isTestnet.ts
+++ src/lib/utils/isTestnet.ts
@@ -1,7 +1,7 @@
-import { TESTNET_ENV_CHAIN_IDS } from '../constants'; 
+import {TESTNET_ENV_CHAIN_IDS} from '../constants'
 
 const isTestnet = (chainId: string) => {
-    return TESTNET_ENV_CHAIN_IDS.includes(chainId);
+  return TESTNET_ENV_CHAIN_IDS.includes(chainId)
 }
 
 export default isTestnet
